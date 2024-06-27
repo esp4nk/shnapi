f53e31bdb831364742aa428aa0a5b526054b71fe
esp4nk <152133108+esp4nk@users.noreply.github.com>
2024-06-27 17:23:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,48 +1,320 @@
-const axios = require('axios');
-const express = require('express');
+import express from "express";
+import Ajv from "ajv";
+import axios from "axios";
+import bodyParser from "body-parser";
 const app = express();
-const port = 3000;
+app.use(bodyParser.json());
+const ajv = new Ajv();
 
-const cors = require('cors');
-app.use(cors({
-    origin: '*'
-}));
+export function convertText(textObj) {
+  return {
+    type: "text",
+    text: textObj.payload.message,
+    buttons: [],
+  };
+}
 
+export function convertImage(imageObj) {
+  return {
+    type: "image",
+    url: imageObj.payload.image,
+    buttons: [],
+  };
+}
 
-app.get('/cpf/:cpf', async (req, res) => {
-    try {
-        const cpf = req.params.cpf;
-        const response = await axios.get(`https://databit.online/api?token=3eaf82ca-0a30-4f14-8313-937c8b26a0f1&type=cpftype&query=${cpf} 
-    `);
-        res.json(response.data);
-    } catch (error) {
-        res.status(500).send('Error retrieving CPF information');
+export function convertUrlButton(buttonObj) {
+  return {
+    type: "url",
+    caption: buttonObj.name,
+    url: buttonObj.request.payload.actions[0].payload.url,
+    webview_size: "full",
+  };
+}
+
+export function convertCardElement(cardObj) {
+  const buttons = [];
+  let actionUrl = null;
+
+  console.log(cardObj);
+  if (cardObj.buttons) {
+    cardObj.buttons.forEach((button) => {
+      if (button.request.payload.actions[0].type.includes("url")) {
+        buttons.push(convertUrlButton(button));
+        if (!actionUrl) {
+          actionUrl = button.request.payload.actions[0].payload.url;
+        }
+      }
+    });
+  }
+  return {
+    title: cardObj.title,
+    subtitle: cardObj.description?.text,
+    image_url: cardObj.imageUrl,
+    action_url: actionUrl,
+    buttons: buttons,
+  };
+}
+
+export function convertCard(cardObj) {
+  return {
+    type: "cards",
+    elements: [convertCardElement(cardObj)],
+    image_aspect_ratio: "horizontal",
+  };
+}
+
+export function convertCarousel(carouselObj) {
+  return {
+    type: "cards",
+    elements: carouselObj.payload.cards.map((el) => convertCardElement(el)),
+    image_aspect_ratio: "horizontal",
+  };
+}
+
+export function convertChoices(choiceObj, contentId) {
+  return (choiceObj.payload?.buttons ?? []).map((button) => {
+    return {
+      type: "flow",
+      caption: button.name,
+      target: contentId,
+    };
+  });
+}
+
+async function checkIfUserHasStarted(userId, voiceflowKey) {
+  const response = await axios(
+    `https://general-runtime.voiceflow.com/state/user/${userId}`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: voiceflowKey,
+      },
+    },
+  );
+  return "stack" in response.data;
+}
+
+function convertVoiceflowResponseToManyChat(
+  voiceflowResponses,
+  showQuickReplies,
+  showReplySuggestions,
+  platform,
+  contentId,
+) {
+  const messages = [];
+  const quick_replies = [];
+  voiceflowResponses.forEach((response) => {
+    const { type, ...payload } = response;
+    if (type === "text") {
+      messages.push(convertText(payload));
+    } else if (type === "visual") {
+      messages.push(convertImage(payload));
+    } else if (type === "cardv2") {
+      messages.push(convertCard(payload));
+    } else if (type === "carousel") {
+      messages.push(convertCarousel(payload));
+    } else if (type === "choice") {
+      const choices = convertChoices(payload, contentId);
+      if (showQuickReplies) {
+        quick_replies.push(
+          ...choices.filter((choice) => choice.caption.length <= 20),
+        );
+      }
+      const allViableReplies = choices.map((r) => r.caption);
+      if (allViableReplies.length && showReplySuggestions) {
+        const text = `
+              Please reply with one of the following:
+    - ${allViableReplies.join("\n  - ")}
+            `;
+        messages.push({
+          type: "text",
+          text: text,
+        });
+      }
     }
+  });
+  if (showQuickReplies && quick_replies.length > 0) {
+    if (messages[messages.length - 1].type != "text") {
+      messages.push({
+        type: "text",
+        text: "What else would you like to do?",
+      });
+    }
+  }
+  const response = {
+    version: "v2",
+    content: {
+      messages: messages,
+      quick_replies: quick_replies,
+    },
+  };
+
+  if (platform && platform != "facebook") {
+    response["type"] = platform;
+    response.content["type"] = platform;
+  }
+  return response;
+}
+
+async function convertToManyChat(body, voiceflowKey) {
+  const {
+    message,
+    intent,
+    userId,
+    showQuickReplies,
+    showReplySuggestions,
+    firstMessagePassthrough,
+  } = body;
+  const userLaunched = await checkIfUserHasStarted(userId, voiceflowKey);
+  if (userLaunched) {
+    const { data } = await axios(
+      `https://general-runtime.voiceflow.com/state/user/${userId}/interact`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: voiceflowKey,
+          Accept: "application/json",
+        },
+        data: {
+          action: {
+            type: intent ? "intent" : "text",
+            payload: intent
+              ? {
+                  query: intent,
+                }
+              : message.replace(/\n/g, " ").trim(),
+          },
+        },
+      },
+    );
+    return convertVoiceflowResponseToManyChat(
+      data,
+      showQuickReplies,
+      showReplySuggestions,
+      body.platform,
+      body.contentId,
+    );
+  } else {
+    const { data } = await axios(
+      `https://general-runtime.voiceflow.com/state/user/${userId}/interact`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: voiceflowKey,
+          Accept: "application/json",
+        },
+        data: {
+          action: {
+            type: "launch",
+          },
+        },
+      },
+    );
+    if (firstMessagePassthrough) {
+      const { data } = await axios(
+        `https://general-runtime.voiceflow.com/state/user/${userId}/interact`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: voiceflowKey,
+            Accept: "application/json",
+          },
+          data: {
+            action: {
+              type: intent ? "intent" : "text",
+              payload: intent
+                ? {
+                    query: intent,
+                  }
+                : message.replace(/\n/g, " ").trim(),
+            },
+          },
+        },
+      );
+      return convertVoiceflowResponseToManyChat(
+        data,
+        showQuickReplies,
+        showReplySuggestions,
+        body.platform,
+        body.contentId,
+      );
+    } else {
+      return convertVoiceflowResponseToManyChat(
+        data,
+        showQuickReplies,
+        showReplySuggestions,
+        body.platform,
+        body.contentId,
+      );
+    }
+  }
+}
+
+const schema = {
+  type: "object",
+  properties: {
+    message: { type: "string" },
+    userId: { type: ["string", "number"] },
+    intent: { type: ["string", "null"] },
+    platform: { type: ["string", "null"] },
+    showQuickReplies: { type: "boolean" },
+    showReplySuggestions: { type: "boolean" },
+    firstMessagePassthrough: { type: "boolean" },
+    contentId: { type: "string" },
+  },
+  required: [
+    "message",
+    "intent",
+    "userId",
+    "showQuickReplies",
+    "showReplySuggestions",
+    "firstMessagePassthrough",
+    "contentId",
+  ],
+};
+const headersSchema = {
+  type: "object",
+  properties: {
+    authorization: { type: "string" },
+  },
+  required: ["authorization"],
+};
+const validate = ajv.compile(schema);
+const validateHeaders = ajv.compile(headersSchema);
+
+app.post("/", async (req, res) => {
+  if (req.method != "POST") {
+    res.status(401).send("Invalid method");
+    return;
+  }
+  if (!validateHeaders(req.headers)) {
+    res.status(403).send("Unauthorized");
+    return;
+  }
+  try {
+    console.log("Request: ", req.body);
+    if (validate(req.body)) {
+      if (typeof req.body.userId == "number") {
+        req.body.userId = `${req.body.userId}`;
+      }
+      const result = await convertToManyChat(
+        req.body,
+        req.headers.authorization,
+      );
+      console.log(result);
+      res.status(200).json(result);
+    } else {
+      res.status(400).send("Invalid body: " + JSON.stringify(validate.errors));
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("ERROR");
+  }
 });
 
-app.get('/ip', async (req, res) => {
-    try {
-        const response = await axios.get(`https://api.ip2location.io/?key=186019B77AAC61D08BDF0BD42F75AB05 
-      `);
-        res.json(response.data);
-    } catch (error) {
-        res.status(500).send('Error retrieving IP information');
-    }
-});
-
-
-app.get('/cep/:cep', async (req, res) => {
-    try {
-        const cep = req.params.cep;
-        const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
-        res.json(response.data);
-    } catch (error) {
-        res.status(500).send('Error retrieving CEP information');
-    }
-});
-
-
-
-app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+app.listen(3000, () => {
+  console.log("Express server initialized");
 });
